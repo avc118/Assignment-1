d5cbb8ef8e870e5023b5defca00d689647716564
angelle cheung <avcheung18@gmail.com>
2015-06-20 16:12:09 -0700

    The original commit for the password project

---
+++ Assignment 1.cpp
@@ -0,0 +1,15 @@
+// Assignment 1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+   cout << "Hello World! Welcome~\n";
+   return 0;
+}
+/*----paste of run ----
+Hello World
+Press any key to continue . . .
+-----------------------*/
